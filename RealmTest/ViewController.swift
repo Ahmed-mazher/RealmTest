e33e372f1e710e0a8dc5ce70deaa620bf1e02257
Ahmed Mazher <ahmedmazher17@gmail.com>
2020-01-26 12:32:29 +0200

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by Rivile on 1/26/20.
+//  Copyright © 2020 Rivile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
